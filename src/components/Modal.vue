cd0db332b6b108e6690c77c96e5798ca0a8e4bbc
jescolargonzalez <jesusescolargonzalez@gmail.com>
2023-06-27 15:09:12 +0200

    añadido y utilizado Modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,13 @@
+<template>
+    <h4> MODAL </h4>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/CustomSlots.vue
+++ src/views/CustomSlots.vue
@@ -1,11 +1,17 @@
 <template>
     <h1>Aprendiendo Slots</h1>
     <button>Abrir Modal</button>
+    <Modal/>
 </template>
 
 <script>
+import Modal from '../components/Modal.vue'
+
 export default {
-    
+    components: { Modal },
+    setup() {
+
+    }
 }
 </script>
 
