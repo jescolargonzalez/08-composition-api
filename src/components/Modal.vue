07e36ee871601e2b59330bc3144858e95d8968ae
jescolargonzalez <jesusescolargonzalez@gmail.com>
2023-06-27 16:01:39 +0200

    slots + modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,13 +1,97 @@
 <template>
-    <h4> MODAL </h4>
+<div class="modal-background fade-in" @click.self="$emit('on:close')"> <!-- con .self no afecta a los hijos el scope-->
+    <div class="modal-container">
+        <hr>
+        <slot name="header"/>
+        <hr>
+       
+        <hr><!-- <slot/> -->
+        <slot name="body">
+            <!--Recibimos el SLOT-->
+            <div class="center"> <!-- Envolver para aplicar css -->
+                Esto aparecera si no tenemos contenido.
+            </div> 
+        </slot>
+        <hr>
+        <hr>
+        <slot name="footer"/>
+        <hr>           
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-
+    props:{
+        title:{
+            type:String,
+            required:true
+        }
+    },
+    emits: ['on:close'],
+// diferencia entre atributo y property : si esta definida en el setup.
+// props se reciben como argumento y pueden ser utilizadas y si no esta definida en el setup se considera atributo
+    setup( props, context ){
+        
+    }
 }
 </script>
 
 <style scoped>
+.modal-background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
 
+    display: flex;
+    background-color: rgba(0, 0, 0, 0.5);
+    align-items: center;
+    justify-content: center;
+}
+.modal-container {
+    width: 250px;
+    height: 250px;
+    background-color: white;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+}
+.center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 250px;
+    height: 250px;
+    color: red;
+}
+
+/*ANIMACIONES*/
+.fade-in {
+  animation: fadeIn ease-out 0.2s;
+  -webkit-animation: fadeIn ease-out 0.2s;
+  -moz-animation: fadeIn ease-out 0.2s;
+  -o-animation: fadeIn ease-out 0.2s;
+  -ms-animation: fadeIn ease-out 0.2s;
+}
+@keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+@-moz-keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+@-webkit-keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+@-o-keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+@-ms-keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
 </style>
--- src/views/CustomSlots.vue
+++ src/views/CustomSlots.vue
@@ -1,16 +1,41 @@
 <template>
-    <h1>Aprendiendo Slots</h1>
-    <button>Abrir Modal</button>
-    <Modal/>
+<h1>Aprendiendo Slots</h1>
+<button @click="openModal">Abrir Modal</button>
+<modal v-if="isOpen" title="Hola mundo" otherTitle="Otro Titulo" @on:close="closeModal"> <!--SLOT-->        
+    <!-- 
+    <h2>Titulo del modal</h2>
+    <p>adifhñsdfhadskhfasdfh</p>
+    <button>Salir</button>
+    -->
+    <template v-slot:header>
+        <h2>Titulo del modal</h2>
+    </template> 
+    <template v-slot:footer>
+        <button @click="closeModal">Salir</button>
+    </template> 
+    <template v-slot:body>
+        <p>adifhñsdfhadskhfasdfh asdsa asd asd asd qwfqw q w</p>
+    </template> 
+</modal>
 </template>
 
 <script>
 import Modal from '../components/Modal.vue'
+import { ref } from 'vue'
 
 export default {
     components: { Modal },
     setup() {
 
+        const isOpen = ref(false)
+
+        return{
+            isOpen,
+            //methods
+            openModal: () => isOpen.value = true,
+            closeModal: () => isOpen.value = false
+
+        }
     }
 }
 </script>
