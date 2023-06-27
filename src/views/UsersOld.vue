8c1a23f6f162b071f2a99048a8f6e141217e7a9b
jescolargonzalez <jesusescolargonzalez@gmail.com>
2023-06-27 17:54:44 +0200

    scoped slots

--- src/views/UsersOld.vue
+++ src/views/UsersOld.vue
@@ -2,31 +2,32 @@
     <h2 v-if="isLoading">Espere por favor...</h2>    
     <h2 v-else>Usuarios</h2>
     <hr>
-    <h5 v-if="errorMessage" class="error">{{ errorMessage }}</h5>
-    <hr>
-
+    <h5 v-if="errorMessage" class="error">{{ errorMessage }}
+        <hr>
+    </h5>
 
     <div v-if="users.length > 0">
-        <UserList :users="users" v-slot="{user}">
-            <h5>{{user.first_name}} - {{user.last_name}}</h5>
-            <span>{{user.email}}</span>
-        </UserList>
+        <ul>
+            <li v-for="{ first_name, last_name, email, id } in users" :key="id"> <!-- Desestruracion :) -->
+                <h3>{{ first_name }} {{ last_name }}</h3>
+                <h4>{{ email }}</h4>
+            </li>
+        </ul>
     </div>
-    <hr>
     <hr>
     <button @click="prevPage">Atras</button>
     <button @click="nextPage">Siguiente</button>
     <span> Página: {{ currentPage }}</span>
-    <hr>
 
 </template>
 
 <script>
 import useUsers from '../composables/useUsers'
-import UserList from '../components/UserList.vue';
+
 export default {
-    components:{ UserList },
+
     setup() {
+
         const { 
             currentPage,
             errorMessage,
